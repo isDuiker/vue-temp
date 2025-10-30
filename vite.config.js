c7b4b966d9fcafd0662ddedd8dc8987e8ca98278
DongHao <3364836519@qq.com>
2025-10-30 18:30:01 +0800

    feat: 新增路径URL转换规则

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
-import vue from '@vitejs/plugin-vue'
-
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
+import vue from "@vitejs/plugin-vue";
+import { resolve } from "path";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), tailwindcss()],
-})
+  resolve: {
+    alias: {
+      "~": resolve(__dirname, "./"),
+      "@": resolve(__dirname, "./src"),
+    },
+  },
+});
